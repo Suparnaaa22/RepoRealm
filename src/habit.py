f66f35e5dc6363cfe3efe8e4df460c13dc27109a
Aritrika Rudra Suparna <aritrikaaa22@gmail.com>
2024-09-30 16:01:27 -0400

    Delete src directory

--- src/habit.py
+++
@@ -1,8 +0,0 @@
-class Habit:
-    def __init__(self, name, frequency):
-        self.name = name
-        self.frequency = frequency
-        self.progress = 0
-
-    def mark_done(self):
-        self.progress += 1
